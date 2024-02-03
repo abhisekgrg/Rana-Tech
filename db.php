2b59855a9c611802f841a1fcdd154cb8f7865067
Rana-tech <abhisek.gurung590@gmail.com>
2024-02-04 01:28:10 +0545

    all files are added

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database_name= "zym";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database_name);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
---
+++ services.php
@@ -0,0 +1,5 @@
+<?php
+include 'header.php';
+
+echo "This is services page";
+?>
