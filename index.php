cc32f85ccf0ca7b0087f29caa7d29028500d442c
Rana-tech <abhisek.gurung590@gmail.com>
2024-02-04 01:40:21 +0545

    rana tech hompage is added

--- index.php
+++ index.php
@@ -4,13 +4,226 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Rana Tech</title>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
     <?php
    include 'header.php';
     ?>
-    <h1>This is homepage</h1>
+    <div class="herosection container flex justify-content align-items">
+        <div class="flex flex-direction justify-content align-items">
+            <p class="text-white rowgap-20">Our IT services are designed to empower your organization, enhance efficiency, and drive innovation</p>
+            <h1 class="text-white text-align">Excellent IT Services <br>
+                for your Success</h1>
+      <button class="button">Learn More</button>
+    </div>
+    </div>
+    <div class="flex justify-content align-items margin container columngap-20">
+        <div class="flex flex-direction justify-content rowgap-10">
+            <h3>About Us</h3>
+            <h2>StartUp Your Business With US</h2>
+            <p>our commitment to excellence in Information Technology is driven by a 
+                passionfor innovation and a dedication to empowering businesses through 
+                cutting-edge solutions. With a team of seasoned professionals at the forefront
+                of technological advancements, we take pride in delivering comprehensive IT 
+                services that not only meet but exceed the evolving needs of our clients.
+                Our journey is defined by a relentless pursuit of excellence, fueled by a desire to 
+                transform challenges into opportunities.</p>
+        </div>
+        <div>
+            <img src="images/2.png" alt="">
+        </div>
+    </div>
+    <div class="services flex flex-direction container section-space  justify-content align-items rowgap-20">
+        <div class="flex  flex-direction align-items">
+            <h3>Services</h3>
+            <h2 class="text-align">We Are Offering All Kinds of<br>
+                IT Solution Services</h2>
+        </div>
+        <div class="serviceslist flex flex-direction justify-content align-items rowgap-20 ">
+            <div class="flex columngap-20 justify-content align-items">
+                <div class="s1 flex  flex-direction align-items">
+                    <img src="images/s1logo.png" alt="">
+                    <h3>Software Development</h3>
+                    <p class="text-align">Web development involves the creation and <br>
+                        maintenance of websites and web <br>
+                        applications.</p>
+                </div>
+                <div class="s1 flex flex-direction justify-content align-items">
+                    <img src="images/s2logo.png" alt="">
+                    <h3>Web Development</h3>
+                    <p class="text-align">Web development involves the creation and<br> 
+                        maintenance of websites and web <br>
+                        applications.</p>
+                </div>
+                <div class="s1 flex flex-direction justify-content align-items">
+                    <img src="images/s3logo.png" alt="">
+                    <h3>Web Development</h3>
+                    <p class="text-align">Web development involves the creation and <br>
+                        maintenance of websites and web <br>
+                        applications.</p>
+                </div>
+            </div>
+            <div class="flex columngap-20  justify-content align-items">
+                <div class="s1 flex  flex-direction align-items">
+                    <img src="images/s4logo.png" alt="">
+                    <h3>Web Development</h3>
+                    <p class="text-align">Web development involves the creation and <br>
+                        maintenance of websites and web <br>
+                        applications.</p>
+                </div>
+                <div class="s1 flex flex-direction align-items">
+                    <img src="images/s5logo.png" alt="">
+                    <h3>Web Development</h3>
+                    <p class="text-align">Web development involves the creation and <br>
+                        maintenance of websites and web <br>
+                        applications.</p>
+                </div>
+                <div class="s1 flex flex-direction align-items">
+                    <img src="images/s6logo.png" alt="">
+                    <h3>Web Development</h3>
+                    <p class="text-align">Web development involves the creation and <br>
+                        maintenance of websites and web <br>
+                        applications.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="reason flext justify-content align-items section-space  rowgap-20 container">
+        <div class="flex flex-direction justify-content align-items">
+            <h3>Why Choose Us</h3>
+            <h2>Reason For Choosing Us </h2>
+            <p class="text-align">Our Company was based on a combination of expertise, reputation<br>
+                innovation, cost-effectiveness, and reliable customer support, all of which align with your project goals and expectations.</p>
+        </div>
+        <div class="flex justify-content align-items columngap-20 ">
+            <div class=" v1 flex flex-direction justify-content align-items ">
+                <img src="images/Vector.png" alt="">
+                <h2>Quality Work</h2>
+                <p class="text-align">Our qualify and experience <br>
+                    member can help to improve your <br>
+                    business</p>
+            </div>
+            <div class="v1 flex flex-direction justify-content align-items">
+                <img src="images/Vector2.png" alt="">
+                <h2>Quality Work</h2>
+                <p class="text-align">Our qualify and experience <br>
+                    member can help to improve your<br> 
+                    business</p>
+            </div>
+            <div class="v1 flex flex-direction justify-content align-items">
+                <img src="images/Vector3.png" alt="">
+                <h2>Quality Work</h2>
+                <p class="text-align">Our qualify and experience <br>
+                    member can help to improve your <br>
+                    business</p>
+            </div>
+            <div class="v1 flex flex-direction justify-content align-items">
+                <img src="images/Vector4.png" alt="">
+                <h2>Quality Work</h2>
+                <p class="text-align">Our qualify and experience<br> 
+                    member can help to improve your <br>
+                    business</p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="flex flex-direction justify-content align-items section-space container">
+            <h3>our expert</h3>
+            <h2>Meet  All Of Our  Expert </h2>
+        </div>
+        <div class="flex justify-content align-item columngap-20">
+            <div class="flex flex-direction justify-content align-items">
+                <img src="images/Ellipse 1.png" alt="">
+                <h3>Roshani Rana Magar</h3>
+                <p>Founder/CEO</p>
+            </div>
+            <div class="flex flex-direction justify-content align-items">
+                <img src="images/Ellipse 2.png" alt="">
+                <h3>Roshani Rana Magar</h3>
+                <p>Founder/CEO</p>
+            </div>
+            <div class="flex flex-direction justify-content align-items">
+                <img src="images/Ellipse 3.png" alt="">
+                <h3>Roshani Rana Magar</h3>
+                <p>Founder/CEO</p>
+            </div>
+            <div class="flex flex-direction justify-content align-items">
+                <img src="images/Ellipse 4.png" alt="">
+                <h3>Roshani Rana Magar</h3>
+                <p>Founder/CEO</p>
+            </div>
+            <div class="flex flex-direction justify-content align-items">
+                <img src="images/Ellipse 5.png" alt="">
+                <h3>Roshani Rana Magar</h3>
+                <p>Founder/CEO</p>
+            </div>
+        </div>
+    </div>
+    <div class="client"></div>
+    <div class="footer flex justify-content container section-space margin " style="column-gap: 90px;">
+        <div>
+            <div>
+                <img src="images/logo.png" alt="">
+            </div>
+            <div> <p>Welcome to work together <br>
+                with RANATECH, your best<br>
+                 IT solution operator and <br>
+                partner in<br> 
+                Nepal!</p></div>
+            <div><h4>Follow Us on :</h4></div>
+            <div class="social icon flex columngap-20"> 
+                <img src="images/f.png" alt="">
+                <img src="images/g.png" alt="">
+                <img src="images/t.png" alt="">
+                <img src="images/l.png" alt="">
+                <img src="images/i.png" alt="">
+            </div>
+        </div>
+        <div class="flex  flex-direction ">
+            <div ><h4>IT Services</h4></div>
+            <div class="flex flex-direction ">
+                <div><p>Web Development</p></div>
+                <div><p>Software Development</p></div>
+                <div><p>Analytical Solution</p></div>
+                <div><p>Digital Marketing</p></div>
+                <div><p>UIUX Designs</p></div>
+                <div><p>Mobile App Design</p></div>
+            </div>
+        </div>
+        <div class="flex  justify-content">
+            <div><h4>Contact Info</h4>
+            <div class="flex flex-direction rowgap-10">
+                <div class="flex align-items columngap-10 ">
+                    <div> <img src="images/Location.png" alt=""></div>
+                    <div><p>Bharatpur-15,Chitwan</p></div>
+                </div>
+                <div class="flex columngap-10 align-items  ">
+                    <div> <img src="images/mingcute_phone-fill.png" alt=""></div>
+                    <div>9862784140<br>
+                        98138329798</div>
+                </div>
+                <div class="flex columngap-10 align-items">
+                    <div> <img src="images/email.png" alt=""></div>
+                    <div><p>rosetravel@gmail.com</p></div>
+                </div>
+                <div class="flex columngap-10 align-items">
+                    <div> <img src="images/clock.png" alt=""></div>
+                    <div><p>Opening Hours: 10:00-4:00</p></div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div> <h4>Sign Up to Newsletter</h4>
+            <p>Sign up to receive emails from us <br>
+                on news,campaigns and events.</p>
+                <div class="flex text-align "> <input type="text" placeholder="your email address">
+                    <img src="images/Group 89.png" alt=""></div>
+               
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
